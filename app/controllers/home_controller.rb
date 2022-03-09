738947d55d04ef09e79d9e9829df100435b74f30
Krish <krishal@myxplor.com>
2022-03-09 19:19:28 +1100

    Added forms for creating listings, edit and destroy methods

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,4 @@
 class HomeController < ApplicationController
-  def home; end
+  def home
+   end
 end
--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,12 +1,59 @@
 class ListingsController < ApplicationController
-  # before_action :set_listing, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_listing, only: [:show, :edit, :update, :destroy]
+  before_action :set_form_vars, only: [:new, :edit]
 
   def index
     @listings = Listing.all
   end
 
-def show
-  @listing = Listing.find(params[:id])
+  def show
+    
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = current_user.listings.new(listing_params)
+    if @listing.save
+      redirect_to @listing, notice: "Listing successfully created"
+    else
+      set_form_vars
+      render "new", alert: "Something went wrong"
+    end
+  
+  end
+
+  def update
+    @listing.update (listing_params)
+    if @listing.save
+      redirect_to @listing, notice: "Listing successfully updated"
+    else
+      set_form_vars
+      render "edit", alert: "Something went wrong"
+    end
+  end
+
+  def destroy
+    @listing.destroy
+  end
+
+  private
+
+def listing_params
+  params.require(:listing).permit(:title, :price, :category_id, :condition, :description)
 end
+
+  def set_listing
+    @listing = Listing.find(params[:id])
+  end
+
+  def set_form_vars
+    @categories = Category.all
+    @conditions = Listing.conditions.keys
+  end
+
 
 end
