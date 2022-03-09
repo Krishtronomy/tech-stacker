4aa1c6ecfdb2b27aacd5ba2493f6eccbf43b0dd4
Krish <krishal@myxplor.com>
2022-03-09 22:23:45 +1100

    Added image upload feature to listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -43,7 +43,7 @@ class ListingsController < ApplicationController
   private
 
 def listing_params
-  params.require(:listing).permit(:title, :price, :category_id, :condition, :description)
+  params.require(:listing).permit(:title, :price, :category_id, :condition, :description, :picture)
 end
 
   def set_listing
